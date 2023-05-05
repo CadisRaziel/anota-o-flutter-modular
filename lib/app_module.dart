ac21de3e9c5d071c9191dc8930d0d221669f5f8d
vitu <vitorcsharp@hotmail.com>
2023-05-04 22:52:02 -0300

    iniciando o modular

---
+++ lib/app_module.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module{
+  //!Toda parte de gerenciamento de dependencia
+  @override
+  List<Bind<Object>> get binds => [];
+
+
+  //!Rotas
+  @override
+  List<ModularRoute> get routes => [];
+}
